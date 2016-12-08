b1902d96ece4d481c72002865cd4c9d58f59572d
Pierre Donias <pierre.donias@gmail.com>
2016-12-08 10:27:06 +0100

    feat(synchronized): default export is a function synchronizer decorator

--- src/index.js
+++ src/index.js
@@ -3,16 +3,52 @@ const toDecorator = wrap => (target, key, descriptor) => {
     return wrap(target)
   }
 
-  return {
-    ...descriptor,
-    value: wrap(descriptor.value)
+  const { value, writable, ...newDescriptor } = descriptor
+  newDescriptor.get = function () {
+    const wrappedMethod = wrap(value, true)
+
+    const descriptor = Object.getOwnPropertyDescriptor(target, key)
+    delete descriptor.get
+    descriptor.value = wrappedMethod
+    if ('set' in descriptor) {
+      delete descriptor.set
+      descriptor.writable = true
+    }
+    Object.defineProperty(this, key, descriptor)
+
+    return wrappedMethod
   }
+  if (writable) {
+    newDescriptor.set = function (value) {
+      const descriptor = Object.getOwnPropertyDescriptor(target, key)
+      delete descriptor.set
+      Object.defineProperty(this, key, descriptor)
+    }
+  }
+
+  return newDescriptor
 }
 
 // ===================================================================
 
-const synchronized = fn => function () {
-  throw new Error('not implemented')
+const synchronized = fn => {
+  const queue = []
+  const dequeue = () => queue.splice(0, 1)
+
+  return function (...params) {
+    const makeCall = () => {
+      const promise = fn.call(this, ...params)
+      queue.push(promise)
+      promise.then(dequeue, dequeue)
+      return promise
+    }
+
+    if (queue.length === 0) {
+      return makeCall()
+    } else {
+      return queue[queue.length - 1].then(makeCall, makeCall)
+    }
+  }
 }
 
 export default toDecorator(synchronized)
--- src/index.spec.js
+++ src/index.spec.js
@@ -3,5 +3,140 @@
 import synchronized from './'
 
 describe('synchronized()', () => {
-  throw new Error('not implemented')
+  it('should synchronize async functions', () => {
+    let i = 0
+    const fn = synchronized((expectedI, result) => {
+      expect(expectedI).toBe(i++)
+
+      return Promise.resolve().then(() => {
+        return result
+      })
+    })
+
+    return Promise.all([
+      fn(0, 'foo').then(result => {
+        expect(result).toBe('foo')
+      }),
+      fn(1, 'bar').then(result => {
+        expect(result).toBe('bar')
+      })
+    ])
+  })
+
+  it('should synchronize async delayed functions', () => {
+    let i = 0
+    const fn = synchronized((expectedI, result) => {
+      return new Promise((resolve, reject) => {
+        const tmp = i
+        expect(expectedI).toBe(tmp)
+
+        setTimeout(() => {
+          i = tmp + 1
+          resolve(result)
+        }, 100)
+      })
+    })
+
+    return Promise.all([
+      fn(0, 'foo').then(result => {
+        expect(result).toBe('foo')
+      }),
+      fn(1, 'bar').then(result => {
+        expect(result).toBe('bar')
+      })
+    ]).then(() => expect(i).toBe(2))
+  })
+
+  it('should synchronize async delayed functions with one rejected promise', () => {
+    let i = 0
+    const fn = synchronized((expectedI, result) => {
+      return new Promise((resolve, reject) => {
+        const tmp = i
+        expect(expectedI).toBe(tmp)
+
+        setTimeout(() => {
+          i = tmp + 1
+          if (i > 1) {
+            reject('reason')
+          } else {
+            resolve('ok')
+          }
+        }, 100)
+      })
+    })
+
+    return Promise.all([
+      fn(0, 'foo'),
+      fn(1, 'bar')
+    ]).catch(error => {
+      expect(i).toBe(2)
+      expect(error).toBe('reason')
+    })
+  })
+
+  it('should synchronize async methods', () => {
+    let i = 0
+    class Test {
+      @synchronized
+      fn (expectedI, result) {
+        return new Promise((resolve, reject) => {
+          const tmp = i
+          expect(tmp).toBe(expectedI)
+
+          setTimeout(() => {
+            i = tmp + 1
+            resolve(result)
+          }, 100)
+        })
+      }
+    }
+
+    const t = new Test()
+    const t2 = new Test()
+
+    t2.fn = function () {}
+
+    return Promise.all([
+      t.fn(0, 'foo').then(result => {
+        expect(result).toBe('foo')
+      }),
+      t.fn(1, 'bar').then(result => {
+        expect(result).toBe('bar')
+      }),
+      t2.fn(0, 'baz').then(result => {
+        expect(result).toBe('baz')
+      })
+    ]).then(() => {
+      expect(i).toBe(2)
+    })
+  })
+
+  it('should synchronize async static methods', () => {
+    let i = 0
+    class Test {
+      @synchronized
+      static fn (expectedI, result) {
+        return new Promise((resolve, reject) => {
+          const tmp = i
+          expect(tmp).toBe(expectedI)
+
+          setTimeout(() => {
+            i = tmp + 1
+            resolve(result)
+          }, 100)
+        })
+      }
+    }
+
+    return Promise.all([
+      Test.fn(0, 'foo').then(result => {
+        expect(result).toBe('foo')
+      }),
+      Test.fn(1, 'bar').then(result => {
+        expect(result).toBe('bar')
+      })
+    ]).then(() => {
+      expect(i).toBe(2)
+    })
+  })
 })
