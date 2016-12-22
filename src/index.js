2d9ab729f9b00d5f182c1cae02debfe72716e558
Julien Fontanet <julien.fontanet@isonoe.net>
2016-12-22 16:19:34 +0100

    fix: registrations interleaved with executions

--- src/index.js
+++ src/index.js
@@ -1,54 +1,30 @@
-const toDecorator = wrap => (target, key, descriptor) => {
-  // function
-  if (key === undefined) {
-    return wrap(target)
-  }
-
-  return {
-    ...descriptor,
-    value: wrap(descriptor.value, typeof target !== 'function')
-  }
-}
+const toDecorator = (wrapFn, wrapMd = wrapFn) =>
+  (target, key, descriptor) => key === undefined
+    ? wrapFn(target)
+    : {
+      ...descriptor,
+      value: (typeof target === 'function' ? wrapFn : wrapMd)(descriptor.value)
+    }
 
 // ===================================================================
 
-const synchronized = (fn, isMethod) => {
-  if (isMethod) {
-    const s = Symbol()
+const synchronizedFn = fn => {
+  let queue = Promise.resolve()
 
-    return function () {
-      const makeCall = () => {
-        const promise = new Promise(resolve => resolve(fn.apply(this, arguments)))
-
-        const free = () => { this[s] = null }
-        this[s] = promise.then(free, free)
-
-        return promise
-      }
-
-      const current = this[s]
-
-      return current
-        ? current.then(makeCall)
-        : makeCall()
-    }
-  }
-
-  let current
-  const free = () => { current = null }
   return function () {
-    const makeCall = () => {
-      const promise = new Promise(resolve => resolve(fn.apply(this, arguments)))
+    const makeCall = () => fn.apply(this, arguments)
 
-      current = promise.then(free, free)
+    return (queue = queue.then(makeCall, makeCall))
+  }
+}
+const synchronizedMd = method => {
+  const s = Symbol()
 
-      return promise
-    }
+  return function () {
+    const makeCall = () => method.apply(this, arguments)
 
-    return current
-      ? current.then(makeCall)
-      : makeCall()
+    return (this[s] = (this[s] || Promise.resolve()).then(makeCall, makeCall))
   }
 }
 
-export default toDecorator(synchronized)
+export default toDecorator(synchronizedFn, synchronizedMd)
